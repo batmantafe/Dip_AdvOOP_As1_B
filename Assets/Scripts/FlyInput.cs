3f3b703c14cf54e73d4465344bea56ade771e31d
Duane Reymond <duane.reymond@gmail.com>
2018-04-16 17:54:41 +1000

    First Push! Level, Frog, and Fly done.

---
+++ Assets/Scripts/FlyInput.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyInput : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody rb;
+
+    private bool movingUp, movingDown, movingLeft, movingRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        movementSpeed = 5f;
+
+        movingDown = true;
+        movingUp = false;
+        movingLeft = false;
+        movingRight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlyMomentum();
+        FlyMovement();
+    }
+
+    void FlyMomentum()
+    {
+        if (movingDown == true)
+        {
+            transform.Translate(-Vector3.up * (movementSpeed * Time.deltaTime));
+
+            //rb.AddForce(-Vector3.up * (movementSpeed * Time.deltaTime));
+        }
+
+        if (movingUp == true)
+        {
+            transform.Translate(Vector3.up * (movementSpeed * Time.deltaTime));
+
+            //rb.AddForce(Vector3.up * (movementSpeed * Time.deltaTime));
+        }
+
+        if (movingLeft == true)
+        {
+            transform.Translate(-Vector3.right * (movementSpeed * Time.deltaTime));
+
+            //rb.AddForce(-Vector3.right * (movementSpeed * Time.deltaTime));
+        }
+
+        if (movingRight == true)
+        {
+            transform.Translate(Vector3.right * (movementSpeed * Time.deltaTime));
+
+            //rb.AddForce(Vector3.right * (movementSpeed * Time.deltaTime));
+        }
+    }
+
+    void FlyMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movingDown = false;
+            movingLeft = false;
+            movingRight = false;
+
+            movingUp = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movingUp = false;
+            movingLeft = false;
+            movingRight = false;
+
+            movingDown = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movingUp = false;
+            movingDown = false;
+            movingRight = false;
+
+            movingLeft = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movingUp = false;
+            movingDown = false;
+            movingLeft = false;
+
+            movingRight = true;
+        }
+    }
+}
---
+++ Assets/Scripts/FrogInput.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogInput : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    public GameObject tongueLength;
+    public float tongueShotSeconds;
+
+    private bool tongueIsShooting;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+
+        rotateSpeed = 20f;
+        tongueShotSeconds = .25f;
+
+        tongueIsShooting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatingFrogHead();
+
+        ShootTongue();
+    }
+
+    void RotatingFrogHead()
+    {
+        if (Input.GetKey(KeyCode.A) && tongueIsShooting == false)
+        {
+            transform.Rotate(Vector3.forward * (rotateSpeed * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.D) && tongueIsShooting == false)
+        {
+            transform.Rotate(-Vector3.forward * (rotateSpeed * Time.deltaTime));
+        }
+    }
+
+    void ShootTongue()
+    {
+        if (Input.GetKey(KeyCode.KeypadEnter))
+        {
+            StartCoroutine(TongueShotTimer());
+        }
+    }
+
+    IEnumerator TongueShotTimer()
+    {
+        tongueIsShooting = true;
+        tongueLength.SetActive(true);
+
+        yield return new WaitForSeconds(tongueShotSeconds);
+
+        tongueIsShooting = false;
+        tongueLength.SetActive(false);
+    }
+}
