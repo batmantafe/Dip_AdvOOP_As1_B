f501cba11b57ff590ffc13b6e51216af55a824f4
Duane Reymond <duane.reymond@gmail.com>
2018-04-19 11:18:31 +1000

    -Added TongueCollision script to Kick FLY from game if Tongue hits it. -Added Lobby script to turn Cursor back on when returned/kicked to Lobby scene.

---
+++ Assets/Scripts/Lobby.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Lobby : MonoBehaviour
+{
+    void Start()
+    {
+        // Turn CURSOR back on when returned/kicked to Lobby.
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TongueCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class TongueCollision : NetworkBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        // Kick FLY if hit by TONGUE
+        if (other.gameObject.name == "Body")
+        {
+            Debug.Log(other.gameObject);
+
+            other.transform.parent.gameObject.GetComponent<NetworkIdentity>().connectionToClient.Disconnect();
+        }
+    }
+}
